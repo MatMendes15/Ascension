b0a5155c429a9d24f4798959751bc71585651341
MatMendes15 <matmendes2004@gmail.com>
2024-11-16 17:57:27 -0300

    Protótipo Ascension

---
+++ src/Loading.py
@@ -0,0 +1,18 @@
+import pygame
+from sys import exit
+
+class Loading(object):
+    def __init__(self):
+        self.background = (0, 0, 0)
+        pygame.font.init()
+        self.font = pygame.font.Font("assets/fontes/PressStart2PFont.ttf", 30)
+        self.fontColor = (230, 230, 230)
+    
+    def load(self, screen):
+        screen.fill(self.background)
+        text = self.font.render("LOADING...", True, self.fontColor)
+        text_pos = (screen.get_width() // 2 - text.get_width() // 2, 300)
+        screen.blit(text, text_pos)
+
+        pygame.display.flip()
+        pygame.time.delay(4000)
